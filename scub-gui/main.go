57fdd544bc37deb98d72794f6e79c8b2d9ab2583
OxumLabs <oxumlabs@proton.me>
2024-10-22 21:51:22 +0530

    added window

---
+++ scub-gui/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	scub := app.New()
+	println(scub)
+
+	// Create a new window
+	swin := scub.NewWindow("Hello Fyne")
+	swin.Resize(fyne.NewSize(720, 480))
+	swin.SetContent(container.NewVBox(
+		widget.NewLabel("Hello, World!"),
+		widget.NewButton("Quit", func() {
+			scub.Quit()
+		}),
+	))
+
+	// Show and run the application
+	swin.ShowAndRun()
+}
